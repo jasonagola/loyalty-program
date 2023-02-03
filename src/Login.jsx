7ff4e9a4e0ec21e57de2677273538db44222fb91
Jason Agola <29072707+jasonagola@users.noreply.github.com>
2023-02-03 13:31:53 -0500

    custom axios

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,16 +1,21 @@
 import React, {useState, useRef, useEffect, useContext} from 'react';
 import AuthContext from './context/authProvider';
-import axios from './context/authProvider';
+import axios from './api/axios'
 import AuthService from './services/authService';
 const LOGIN_URL = '/auth';
+const devURL = 'http://localhost:8800'
+const buildURL = 'https://jasonagola.dev'
+const backendURL = devURL
 
 function Login() {
     const { setAuth } = useContext(AuthContext)
 
     const errRef = useRef()
 
-    const usernameRef = useRef()
-    const passwordRef = useRef()
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    // const usernameRef = useRef()
+    // const passwordRef = useRef()
 
     const [message, setMessage] = useState()
     const [success, setSuccess] = useState(false)
@@ -22,13 +27,24 @@ function Login() {
 
     const handleLogin = async (e) => {
         e.preventDefault();
+        // const options = {
+        //     method: 'POST',
+        //     url: backendURL + LOGIN_URL,
+        //     data: {
+        //         username, password
+        //     },
+        //     // headers: 
+        //     //     {"Content-Type": "application.json"},
+        //     //     withCredentials: true
+        // }
+        // const response = await axios.request(options)
         try {
             const repsonse = await axios.post(
                 LOGIN_URL,
-                JSON.stringify({usernameRef, passwordRef}),
+                JSON.stringify({username, password}),
                 {
                     headers: { 'Content-Type': 'application.json'},
-                    withCredentials
+                    withCredentials: true
                 });
             console.log(JSON.stringify(response?.data))
             const accessToken = response?.data?.accessToken;
@@ -36,11 +52,11 @@ function Login() {
             setAuth(username, password, roles, accessToken)
         } catch (error) {
             console.log(error)
-            if (!err?.repsonse) {
+            if (!error?.response) {
                 setMessage('No Server Response')
-            } else if (err.response?.status === 400) {
+            } else if (error.response?.status === 400) {
                 setMessage('Missing Username or Password');
-            } else if (err.response?.status === 401) {
+            } else if (error.response?.status === 401) {
                 setMessage('Username or Password Does Not Match');
             } else {
                 setMessage('Login Failed')
@@ -63,9 +79,9 @@ function Login() {
         <div id='login'>
             <form onSubmit={handleLogin}> 
                 <label htmlFor='username'>Username:</label>
-                <input type='text' id='username' name='username' ref={usernameRef} required={true}></input>
+                <input type='text' id='username' name='username' value={username} autoComplete="off" onChange={(e) => setUsername(e.target.value)} required={true}></input>
                 <label htmlFor='password'>Password:</label>
-                <input type='password' id='password' name='password' ref={passwordRef} required={true}></input>
+                <input type='password' id='password' name='password' value={password} autoComplete="off" onChange={(e) => setPassword(e.target.value)}  required={true}></input>
                 <p ref={errRef} className={message ? "errmsg":"offscreen"}>{message}</p>
                 <button>Log In</button>
             </form>
--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,5 @@
-import axios from 'axios'
-
+import axios from 'axios';
 
 export default axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_URL
+    baseURL: 'http://localhost:3500'
 });
-
